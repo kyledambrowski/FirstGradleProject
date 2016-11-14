42e21f601b6ae49d881509ed3503964c8d6c243e
Kyle Dambrowski <kyle.dambrowski@gmail.com>
2016-11-14 14:07:48 -0500

    Commit SomeClass

---
+++ src/main/java/SomeClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by kdoodler on 11/14/2016.
+ */
+public class SomeClass {
+
+    public static void main(String []args){
+        System.out.println("First Gradle Project");
+    }
+
+}
