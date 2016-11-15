b66cb5a822296303625ec1b8aa031323487aaf54
Kyle Dambrowski <kyle.dambrowski@gmail.com>
2016-11-15 11:12:51 -0500

    Commit changes in SomeClass

--- src/main/java/SomeClass.java
+++ src/main/java/SomeClass.java
@@ -4,7 +4,9 @@
 public class SomeClass {
 
     public static void main(String []args){
-        System.out.println("First Gradle Project");
+        System.out.println("First");
+        System.out.println("Gradle");
+        System.out.println("Project");
     }
 
 }
